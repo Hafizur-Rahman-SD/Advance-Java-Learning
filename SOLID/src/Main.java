45237b7ac6b857896419352df9e0ec06d32164d5
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-11-12 22:49:05 +0600

    update

---
+++ SOLID/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// we are learning java 25 update idea
+void main() { // r kichu likhar dorkar nai
+    System.out.println("Hello World 1");
+
+//IO.println("Hello World I am Hafizur Rahman ");
+
+    String Line=IO.readln("What is your name?"); //from take user input
+    System.out.println("Print line result:" +Line); //store the value
+
+    int age=Integer.parseInt(IO.readln("What is your age?"));
+    System.out.println("My name is:"+Line +"\n My age is:" +age + " years old");
+}
+
---
+++ SOLID/src/srp/UserService.java
@@ -0,0 +1,15 @@
+package srp;
+
+public class UserService {
+    public void createUser(String username,String email,String password){
+
+    }
+
+    public void loginUser(String username,String password){
+
+    }
+
+    public void delete(String username){
+
+    }
+}
