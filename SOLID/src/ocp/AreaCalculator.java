a56a15bea14b5770948bde49e7eaf3cea3de11af
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-11-15 10:47:01 +0600

    s

---
+++ SOLID/src/ocp/AreaCalculator.java
@@ -0,0 +1,26 @@
+/*public class AreaCalculator  {
+public double CalculatorArea (double a,double b,double c) {
+    double Area=(a*b*c);
+    return Area;
+
+}
+
+public double CalculatorArea2(double r) {
+    return 3.14*r*r;
+}
+}
+*///Its violate OCP (open Close principal er moto kore kaj hoyni ai jaygay
+//unccel bob chay na amra amne code kori tail code sondor dekhay na
+
+
+public class AreaCalculator  {
+public double CalculateArea(String type,double a,double b,double c) {
+    if (type.equals("TraiaAngel")) {
+        return a*b*c;
+    }else if (type.equals("Circle")) {
+        return 3.14*a*a;
+    }
+return 0;
+}
+}
+
--- SOLID/src/srp/UserService.java
+++ SOLID/src/srp/UserService.java
@@ -1,6 +1,14 @@
 package srp;
 
 public class UserService {
+
+    //srp violation hocceh ai jaygay course related jini alada
+    // class e rakha lagbe clean code likhar jonno  ai jaygay o code thik moto kaj korbw kintu
+    //amra amne ode korbo na taile SOLID foloow kore na
+//    public  void createCourse(String courseName){
+//
+//    }
+
     public void createUser(String username,String email,String password){
 
     }
